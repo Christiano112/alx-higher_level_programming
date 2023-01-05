f4c3b2f6e7a1e239b6a8d486e370156dff6f2043
Christiano112 <enyiachristian12@gmail.com>
2023-01-05 15:37:22 -0800

    third commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+
     i = len(sys.argv) - 1
 
     if i == 0:
@@ -10,9 +11,5 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(i))
 
-    if i >= 1:
-        i = 0
-        for arg in sys.argv:
-            if i != 0:
-                print("{}: {}".format(i, arg))
-                i += 1
+   for e in range(i):
+       print("{}: {}".format(e + 1, sys.argv[e + 1]))

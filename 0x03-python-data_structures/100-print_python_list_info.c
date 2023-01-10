3394fc6836216c50e5ec41b3808324f8d558b2e1
Christiano112 <enyiachristian12@gmail.com>
2023-01-09 17:30:39 -0800

    fixing bugs

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -3,6 +3,7 @@
 
 /**
  * print_python_list_info - prints python list info
+ *
  * @p: python object
  * Return: no return
  */
@@ -15,6 +16,9 @@ void print_python_list_info(PyObject *p)
 
 	size = Py_SIZE(p);
 	printf("[*] Size of the Python LIst = %ld\n", size);
+
+	list = (PyListObject *)p;
+	printf("[*] Allocated = %ld\n", list->allocated);
 
 	for (i = 0; i < size; i++)
 	{

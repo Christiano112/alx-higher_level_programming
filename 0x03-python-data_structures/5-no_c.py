a9ccc5ad63f6734de537c7c7c7a5113b2bea2261
Christiano112 <enyiachristian12@gmail.com>
2023-01-09 17:43:21 -0800

    fixing bugs

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     new_string = ""
     for x in my_string:
-        if x != 'c' and x != "C":
-        new_string += x
+        if x != "c" and x != "C":
+        updated_string += x
 
     return(new_string)

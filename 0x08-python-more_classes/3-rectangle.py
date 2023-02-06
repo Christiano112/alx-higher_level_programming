b22dadf90220957208b7a3ae55b696b05263b75f
Christiano112 <enyiachristian12@gmail.com>
2023-02-05 20:34:21 -0800

    done with the task

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -52,5 +52,5 @@ class Rectangle:
         string = ""
         if self.__width != 0 and self.__height != 0:
             string += "\n".join("#" * self.width
-                    for j in range(sel.__height))
+                    for j in range(self.__height))
         return string

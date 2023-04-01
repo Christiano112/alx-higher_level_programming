9f31517b0bb6f192dfcea1c18bdb5798a3cf40cc
Christiano112 <enyiachristian12@gmail.com>
2023-04-01 05:17:39 -0700

    fixing erros

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,4 +1,4 @@
 -- dsiplays the top 3 cities with highest temperature
 -- during July and August ordered by temperature descending
 
-SELECT city, AVG(value) AS avg_temp FROM temperature WHERE month = 7 OR month = 8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month = 7 OR month = 8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- lists the number of records having the same score
 
-SELECT score, COUNT(*) AS number FROM second_table GROUP_BY score ORDER BY number DESC;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;

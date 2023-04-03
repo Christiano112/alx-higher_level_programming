ac15b3a212230c7b67e090e6854228424e99381b
Christiano112 <enyiachristian12@gmail.com>
2023-04-03 10:32:01 -0700

    fixed incorrect file name

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- lists all genres from the database along with the numbers linked
 
 SELECT g.`name` AS `genre`,
-COUNT(*) AS number_of_shows`
+COUNT(*) AS `number_of_shows`
 FROM `tv_genres` AS g
 INNER JOIN `tv_show_genres` AS t
 ON g.`id` = t.`genre_id`

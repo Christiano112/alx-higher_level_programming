513f447cdb72c8bb359416b04e3c9a80153fb0f7
Christiano112 <enyiachristian12@gmail.com>
2023-04-11 08:32:56 -0700

    more tasks

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+const myArg = parseInt(process.argv[2]);
+let myX = '';
+if (!isNaN(myArg)) {
+  for (let i = 0; i <= myArg; i++) {
+    myX += 'X';
+    console.log(myX);
+  }
+} else {
+  console.log('Missing size');
+}
--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,6 @@
 #!/usr/bin/node
+
+function add(a, b) {
+  return console.log(a + b);
+}
+add (process.argv[2], process.argv[3]);
